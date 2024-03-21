99c3ae5ca7271efdb5d3750f7fc394877094d483
12_7-Đặng Chí Thành <chithanh080804@gmail.com>
2024-03-21 22:42:26 +0700

    khoi tao project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1>Awesome UIT Projects</h1>
+      <div>
+        <h2>Mục lục</h2>
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
