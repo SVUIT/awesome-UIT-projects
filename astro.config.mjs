c08565d72f4ceaafde43fed85a73d68de5ec052f
12_7-Đặng Chí Thành <chithanh080804@gmail.com>
2024-04-07 23:53:53 +0700

    new1

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
-			favicon: '/src/assets/favicon.ico',
+			favicon: '/favicon.svg',
 			customCss:[
 				'/src/styles/custom.scss',
 			],
